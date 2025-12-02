773f29e89645fabe56b7c6a76e533efcab0f0125
Himanshu <himanshu.b@moondive.co>
2025-12-02 12:05:20 +0530

    feature/home

--- app/hrms/login/page.jsx
+++ app/hrms/login/page.jsx
@@ -2,34 +2,11 @@
 import LoginForm from '@/components/Loginpage/LoginForm'
 import ForgotModal from '@/components/Loginpage/ForgotModal'
 import React, { useState } from 'react'
-import { GoogleAuthProvider } from 'firebase/auth'
-import { auth } from '@/lib/firebase'
 
 const page = () => {
   const [showForgotModal, setShowForgotModal] = useState(false);
     const [email, setEmail] = useState("");
  const bgImage = "/mnt/data/f74cd845-284d-46a6-b45b-6b71300493c8.png";
-  const googleProvider = new GoogleAuthProvider();
- 
-   const handleGoogleSignup = async () => {
-     try {
-       const result = await signInWithPopup(auth, googleProvider);
-       const user = result.user;
- 
-       // Extract user data from Google and show modal
-       const googleData = {
-         firstName: user.displayName?.split(" ")[0] || "",
-         lastName: user.displayName?.split(" ")[1] || "",
-         email: user.email || "",
-         acceptedTerms: false,
-       };
- 
-       setGoogleUserData(googleData);
-       setShowGoogleModal(true);
-     } catch (error) {
-       console.error("Google Sign-in Error:", error);
-     }
-   };
   return (
     <div className='relative'>
     <LoginForm
