7364cf4b595d675c613827225d072022df1d5cf5
Adarshvermamd <adarsh.verma@moondive.co>
2025-12-02 18:19:39 +0530

    Changes done

--- app/cms/login/page.jsx
+++ app/cms/login/page.jsx
@@ -13,7 +13,7 @@ const page = () => {
         email={email}
         setEmail={setEmail}
         setShowForgotModal={setShowForgotModal}
-        redirectTo = "/cms"
+        redirectTo = "cms"
       />
       {
 
