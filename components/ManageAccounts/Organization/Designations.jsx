7391a9ea007722f09831b33d8783afc932610355
ubaid.Moon.Dive <Ubaid.A@moondive.co>
2025-12-10 13:43:16 +0530

    changes

--- components/ManageAccounts/Organization/Designations.jsx
+++ components/ManageAccounts/Organization/Designations.jsx
@@ -1,3 +1,22 @@
+'use client'
+import { useState,useEffect } from "react";
+import axios from "axios";
+
+
+
 export default function Designations(){
+    const [designations,setDesignations]=useState(null)
+
+    useEffect(()=>{
+        const fetchDesignation=async()=>{
+            try{
+                const res=await axios.get("/hrms/organization/get-alldesignation")
+                console.log("----------------",res?.data?.result)
+            }catch(err){
+                console.log("error while fetching designation data",err)
+            }
+        }
+        fetchDesignation()
+    },[])
     return <div>Designations Component</div>;
 }
