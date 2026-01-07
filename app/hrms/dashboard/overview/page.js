036eb3f18025d7d28d8ebdd2ae38d248b864a272
Adarshvermamd <adarsh.verma@moondive.co>
2026-01-07 10:20:21 +0530

    Changes done.

--- app/hrms/dashboard/overview/page.js
+++ app/hrms/dashboard/overview/page.js
@@ -1,5 +1,19 @@
+'use client'
+
+import { AuthContext } from "@/context/authContext";
 import { redirect } from "next/navigation";
+import { useContext } from "react";
 
 export default function OverviewPage() {
-  redirect("/hrms/dashboard/overview/myspace");
+  const {user} = useContext(AuthContext)
+
+  if(!user.userRole.includes("SuperAdmin")){
+    redirect("/hrms/dashboard/overview/myspace")
+  }
+
+  return (
+    <div>
+      overview
+    </div>
+  )
 }
