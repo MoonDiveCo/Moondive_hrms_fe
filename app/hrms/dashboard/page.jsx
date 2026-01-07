12d86097dab83fc1f573b0c88cb06c59de4bf615
Adarshvermamd <adarsh.verma@moondive.co>
2026-01-07 10:32:38 +0530

    Changes done.

--- app/hrms/dashboard/page.jsx
+++ app/hrms/dashboard/page.jsx
@@ -1,5 +1,43 @@
-import { redirect } from "next/navigation";
+'use client';
+
+import { AuthContext } from "@/context/authContext";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 export default function OverviewPage() {
-  redirect("/hrms/dashboard/overview/myspace");
+  const { user, loading } = useContext(AuthContext);
+  const router = useRouter();
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    if (loading) return;
+
+    if (!user) {
+      setChecked(true);
+      return;
+    }
+
+    if (!user.userRole?.includes("SuperAdmin")) {
+      router.replace("/hrms/dashboard/overview/myspace");
+      return;
+    }
+
+    setChecked(true);
+  }, [user, loading, router]);
+
+  if (!checked) {
+    return (
+      <div className='flex items-center justify-center h-screen fixed inset-0 bg-black/5 backdrop-blur-sm'>
+        <DotLottieReact
+          src='https://lottie.host/ae5fb18b-4cf0-4446-800f-111558cf9122/InmwUHkQVs.lottie'
+          loop
+          autoplay
+          style={{ width: 100, height: 100 }}
+        />
+      </div>
+    );
+  }
+
+  return <div>overview</div>;
 }
